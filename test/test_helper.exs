26a74a021169ff63cf0026b5e47b3c2c9527ced0
Jason Kimmerling <jkimmerling@protonmail.com>
2025-08-18 15:20:51 -0500

    Fix code formatting for push

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,8 @@ ExUnit.start()
 
 # Start PubSub for tests that need it (if not already started)
 unless Process.whereis(TheMaestro.PubSub) do
-  {:ok, _} = Supervisor.start_link([{Phoenix.PubSub, name: TheMaestro.PubSub}], strategy: :one_for_one)
+  {:ok, _} =
+    Supervisor.start_link([{Phoenix.PubSub, name: TheMaestro.PubSub}], strategy: :one_for_one)
 end
 
 # Only configure Ecto if the repo is available
