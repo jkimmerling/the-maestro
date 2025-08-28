ba2fbabe9473bc4b53da2328afc5e0a43e2d0408
Jason Kimmerling <jkimmerling@protonmail.com>
2025-08-28 11:46:34 -0500

    feat: add Tesla and Finch dependencies for HTTP client
    - Add tesla ~> 1.11 for middleware-based HTTP client
    - Add finch ~> 0.19 for connection pooling adapter
    - Dependencies align with story 1.1 requirements for multi-provider API communication
    
    Story: 1.1 Task 1 ✅

--- mix.exs
+++ mix.exs
@@ -66,6 +66,8 @@ defmodule TheMaestro.MixProject do
        depth: 1},
       {:swoosh, "~> 1.16"},
       {:req, "~> 0.5"},
+      {:tesla, "~> 1.11"},
+      {:finch, "~> 0.19"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
