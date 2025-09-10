a05de3c412345d8f1f896ceb3f053f92c5057443
Jason Kimmerling <jkimmerling@protonmail.com>
2025-09-10 05:17:42 -0500

    fix(auth): SavedAuthentication changeset should only require expires_at for :oauth; validate_oauth_expiry accordingly

--- lib/the_maestro/auth/saved_authentication.ex
+++ lib/the_maestro/auth/saved_authentication.ex
@@ -18,6 +18,17 @@ defmodule TheMaestro.Auth.SavedAuthentication do
   def changeset(saved_authentication, attrs) do
     saved_authentication
     |> cast(attrs, [:provider, :auth_type, :name, :credentials, :expires_at])
-    |> validate_required([:provider, :auth_type, :name, :expires_at])
+    |> validate_required([:provider, :auth_type, :name, :credentials])
+    |> validate_oauth_expiry()
+  end
+
+  defp validate_oauth_expiry(%Ecto.Changeset{} = changeset) do
+    auth_type = get_field(changeset, :auth_type)
+    expires_at = get_field(changeset, :expires_at)
+
+    case {auth_type, expires_at} do
+      {:oauth, nil} -> add_error(changeset, :expires_at, "can't be blank for OAuth tokens")
+      _ -> changeset
+    end
   end
 end
